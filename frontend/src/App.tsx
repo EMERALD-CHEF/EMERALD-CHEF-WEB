6e2b70fc80445a6f2e48d16dfe8b20c2e0d90750
James Miller <miller.8855@osu.edu>
2022-03-04 14:35:09 -0500

    Update for Chad

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import { Route, BrowserRouter as Router, Switch, Link } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Routes, Link } from 'react-router-dom';
 import Product from './Products/Products';
 import Home from './Home/Home';
+import data from './data';
 
 function App() {
   const openMenu = () => {
@@ -13,50 +14,29 @@ function App() {
   const closeMenu = () => {
     document.querySelector(".sidebar")?.classList.remove("open");
   }
-  return (
-      <Router>
-    <div className="grid-container">
-    <link rel="stylesheet" href="style.css"></link>
-    <title>Emerald Chef</title>
-<body>
-    <div>
-        <header className="header">
-            <div className="brand">
-                <button onClick={openMenu}>&#9776;</button>
-                <a href="index.html">Emerald Chef</a>
-            </div>
-            <div className="header-links">
-                <Link to="/">Home</Link>
-                <Link to="/catalog">Catalog</Link>
-            </div>
-        </header>
-        <aside className="sidebar">
-            <h3>Shopping Categories</h3>
-            <button className="sidebar-close-button" onClick={closeMenu}>x</button>
-            <ul>
+  return(
+      <div className = "content">
+          <ul className="products">
+            {
+              data.products.map(product=> 
                 <li>
-                    <a href="index.html">Pants</a>
+                    <div className="product">
+                        <img className="product-image" src={product.imageUrl} alt="product"/>
+                        <div className="product-name">
+                            <a href="product.html">{product.name}</a>
+                        </div>
+                        <div className="product-brand">{product.brand}</div>
+                        <div className="product-price">{product.price}</div>
+                        <div className="product-rating">{product.rating} Stars ({product.numberOfReviews} Reviews) </div>
+                    </div>
                 </li>
-
-                <li>
-                    <a href="index.html">Shirts</a>
-                </li>
-            </ul>
-        </aside>
-        <main className="main">
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/catalog" element={<Products />} />
-            </Routes>
-        </main>
-        <footer>
-            &copy; 2022 Emerald Chef
-        </footer>
-    </div>
-</body>
-</div>
-</Router>
-  );
+                
+                )  
+            }
+          </ul>
+      </div>
+  )
+      
 }
 
 export default App;
---
+++ frontend/src/data.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import data from './data';
+
+
+export default{
+    products: [{
+        name: 'Shirt',
+        imageUrl: '/images/d1.jpg',
+        price: 60,
+        brand: 'Nike',
+        rating: 4.5,
+        numberOfReviews: 10,
+    },
+    {
+        name: 'Shirt',
+        imageUrl: '/images/d1.jpg',
+        price: 60,
+        brand: 'Nike',
+        rating: 4.5,
+        numberOfReviews: 10,
+    },
+    {
+        name: 'Shirt',
+        imageUrl: '/images/d1.jpg',
+        price: 60,
+        brand: 'Nike',
+        rating: 4.5,
+        numberOfReviews: 10,
+    }]
+}
