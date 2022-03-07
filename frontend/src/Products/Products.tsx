95ff4924200484b789048cdcc86d68f0a60e54ab
Benjamin Blackfield <blackfield.2@buckeyemail.osu.edu>
2022-03-07 16:17:08 -0500

    Updated catalog format

--- frontend/src/Products/Products.tsx
+++ frontend/src/Products/Products.tsx
@@ -3,23 +3,24 @@ import data from '../data';
 
 function Products() {
     return (
-        <div>
-            <h2>Products</h2>
-            {data.products.map((product, index) => {
-                return (
-                  <li key={index}>
-                      <div className="product">
-                          <img className="product-image" src={product.imageUrl} alt="product"/>
-                          <div className="product-name">
-                              <a href="product.html">{product.name}</a>
-                          </div>
-                          <div className="product-brand">{product.brand}</div>
-                          <div className="product-price">{product.price}</div>
-                          <div className="product-rating">{product.rating} Stars ({product.numberOfReviews} Reviews) </div>
-                      </div>
-                  </li>
+        <div className="content">
+            <div className="products">
+                {data.products.map((product, index) => {
+                    return (
+                    <li key={index}>
+                        <div className="product">
+                            <img className="product-image" src={product.imageUrl} alt="product"/>
+                            <div className="product-name">
+                                <a href="product.html">{product.name}</a>
+                            </div>
+                            <div className="product-brand">{product.brand}</div>
+                            <div className="product-price">{product.price}</div>
+                            <div className="product-rating">{product.rating} Stars ({product.numberOfReviews} Reviews) </div>
+                        </div>
+                    </li>
+                    )}
                 )}
-              )}
+              </div>
         </div>
     );
 }
