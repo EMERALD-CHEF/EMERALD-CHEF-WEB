f5b1c5889545e10e8677ee079d287cc0bc2c1e54
Olivia Shroyer <shroyerolivia@gmail.com>
2022-04-18 18:50:57 -0400

    made it to 2. add cors support

--- frontend/src/Products/Products.tsx
+++ frontend/src/Products/Products.tsx
@@ -1,11 +1,40 @@
-import React from 'react';
-import data from '../data';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+type Product = {
+    id: number;
+    name: string;
+    description: string;
+    brand: string;
+    price: number;
+    rating: number;
+    numberOfReviews: number;
+    imageUrl: string;
+}
+
+const emptyProducts: Product[] = [];
 
 function Products() {
+    const [products, setProducts]: [Product[], (products : Product[]) => void]
+        = useState(emptyProducts);
+
+    useEffect(() => {
+        // is this the right number?
+        axios.get<Product[]>("https://localhost:7248/catalog",
+        {
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+        // getting an error saying ';' expected on line 25:9
+        .then((response)=> setProducts(response.data))
+        .catch((error)=> console.log(error));
+    }, []);
+    
     return (
         <div className="content">
             <div className="products">
-                {data.products.map((product, index) => {
+                {products.map((product, index) => {
                     return (
                     <li key={index}>
                         <div className="product">
